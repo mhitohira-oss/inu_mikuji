75e6550f3ab902d32509968abeb850a89e9c441a
mhitohira-oss <mhitohira@gmail.com>
2026-04-04 22:09:24 +0900

    first-commit

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const button = document.querySelector(".btn");
+const dog = document.querySelector(".dog-photo");
+const result = document.querySelector(".result");
+
+const fortunes = ["大吉", "中吉", "小吉", "吉", "末吉", "凶"];
+
+button.addEventListener("click", () => {
+  button.disabled = true;
+
+  dog.classList.remove("jump");
+  void dog.offsetWidth;
+  dog.classList.add("jump");
+
+  result.textContent = "・・・";
+
+  const randomIndex = Math.floor(Math.random() * fortunes.length);
+
+  setTimeout(() => {
+    result.textContent = fortunes[randomIndex];
+    button.disabled = false;
+  }, 1500);
+});
