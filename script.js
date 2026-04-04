7560bd08bb454d671089e85acca141fd5e96eb67
mhitohira-oss <mhitohira@gmail.com>
2026-04-04 22:50:06 +0900

    微調整

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const result = document.querySelector(".result");
 const fortunes = ["大吉", "中吉", "小吉", "吉", "末吉", "凶"];
 
 button.addEventListener("click", () => {
+  // アニメーション中の連打防止
   button.disabled = true;
 
+  // アニメーションをリセットして再実行
   dog.classList.remove("jump");
   void dog.offsetWidth;
   dog.classList.add("jump");
@@ -16,7 +18,7 @@ button.addEventListener("click", () => {
   result.textContent = "・・・";
 
   const randomIndex = Math.floor(Math.random() * fortunes.length);
-
+  
   setTimeout(() => {
     result.textContent = fortunes[randomIndex];
     button.disabled = false;
